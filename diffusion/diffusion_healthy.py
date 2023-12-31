9207f951d1f3b38f5b5874578bcc261e48dcd9e0
Lim En Xi <limenxi07@gmail.com>
2023-12-31 15:18:28 +0800

    changed image config

--- diffusion/diffusion_healthy.py
+++ diffusion/diffusion_healthy.py
@@ -19,15 +19,15 @@ import glob
 class TrainingConfig:
     image_size = 128  # the generated image resolution
     train_batch_size = 16
-    eval_batch_size = 16  # how many images to sample during evaluation
+    eval_batch_size = 500  # how many images to sample during evaluation
     num_epochs = 50
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
     lr_warmup_steps = 500
-    save_image_epochs = 10
+    save_image_epochs = 50
     save_model_epochs = 60 # I chose to not save the model as the file size is too large for GitHub
     mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
-    output_dir = "../results/diffusion_healthy"  # the model name locally and on the HF Hub
+    output_dir = "diffusion-healthy"  # the model name locally and on the HF Hub
 
     push_to_hub = False  # whether to upload the saved model to the HF Hub
     hub_private_repo = False
@@ -99,13 +99,13 @@ def evaluate(config, epoch, pipeline):
     ).images
 
     # Make a grid out of the images
-    image_grid = make_grid(images, rows=4, cols=4)
+    # image_grid = make_grid(images, rows=4, cols=4)
 
     # Save the images
-    test_dir = os.path.join(config.output_dir, "samples")
+    test_dir = '../results/diffusion_healthy'
     os.makedirs(test_dir, exist_ok=True)
-    image_grid.save(f"{test_dir}/{epoch:04d}.png")
-    print('number of images: ', len(images))
+    for i in range(len(images)):
+      images[i].save(f"{test_dir}/Healthy-{i}.jpg")
 
 # MAIN TRAINING LOOP
 def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
