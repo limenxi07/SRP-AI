254f30c53478ec08e276f4f4fcb3aa8a74af8cd7
Lim En Xi <limenxi07@gmail.com>
2024-01-01 16:11:14 +0800

    initial commit

--- image_resizer.py
+++ image_resizer.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 path = 'images/B_training_set/'
-destination = 'images/E_square_training_set/'
+destination = 'images/E_square_healthy/'
 
 for image in os.listdir(path):
   im = Image.open(path + image)
