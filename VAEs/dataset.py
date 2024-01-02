d8cfda55ce2941af5677d10734f6a7638f83036c
Lim En Xi <limenxi07@gmail.com>
2024-01-02 17:02:39 +0800

    removed transforms

--- VAEs/dataset.py
+++ VAEs/dataset.py
@@ -7,8 +7,6 @@ from torchvision.datasets.folder import default_loader
 from torchvision.io import read_image
 from lightning import LightningDataModule
 from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
-from torchvision.datasets import CelebA
 import zipfile
 import pandas as pd
 
@@ -56,7 +54,7 @@ class VAEDataset(LightningDataModule):
         self.pin_memory = pin_memory # whether prepared items should be loaded into pinned memory
 
     def setup(self, stage: Optional[str] = None) -> None:
-        transform = transforms.Normalize([0.5], [0.5])
+        transform = None
         
         self.train_dataset = MyDataset(
             data_dir=self.data_dir,
