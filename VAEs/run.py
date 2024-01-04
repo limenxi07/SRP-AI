b0d54302f53a510b4ea089d994cada60e5b3c330
Lim En Xi <limenxi07@gmail.com>
2024-01-04 14:44:26 +0800

    troubleshooting

--- VAEs/run.py
+++ VAEs/run.py
@@ -17,29 +17,27 @@ from torchvision import datasets, transforms
 path = 'images'
 output = 'vae_healthy'
 
-# CUSTOM DATASET
-mnist_trainset = datasets.MNIST(root='../data', train=True, download=True, transform=None)
+# # CUSTOM DATASET
+# mnist_trainset = datasets.MNIST(root='../data', train=True, download=True, transform=None)
 
-train_dataset = mnist_trainset.data[:10000].reshape(-1, 1, 28, 28) / 255.
-train_targets = mnist_trainset.targets[:10000]
-eval_dataset = mnist_trainset.data[-10000:].reshape(-1, 1, 28, 28) / 255.
-eval_targets = mnist_trainset.targets[-10000:]
+# train_dataset = mnist_trainset.data[:10000].reshape(-1, 1, 28, 28) / 255.
+# train_targets = mnist_trainset.targets[:10000]
+# eval_dataset = mnist_trainset.data[-10000:].reshape(-1, 1, 28, 28) / 255.
+# eval_targets = mnist_trainset.targets[-10000:]
 
-for i in range(len(train_dataset)):
-    img = 255.0*train_dataset[i][0].unsqueeze(-1)
-    img_folder = os.path.join("images", "healthy", f"{train_targets[i]}")
-    if not os.path.exists(img_folder):
-        os.mkdir(img_folder)
-    imageio.imwrite(os.path.join(img_folder, "%08d.jpg" % i), np.repeat(img, repeats=3, axis=-1).type(torch.uint8))
-    print('train: ', i)
+# for i in range(len(train_dataset)):
+#     img = 255.0*train_dataset[i][0].unsqueeze(-1)
+#     img_folder = os.path.join("images", "healthy", f"{train_targets[i]}")
+#     if not os.path.exists(img_folder):
+#         os.mkdir(img_folder)
+#     imageio.imwrite(os.path.join(img_folder, "%08d.jpg" % i), np.repeat(img, repeats=3, axis=-1).type(torch.uint8))
 
-for i in range(len(eval_dataset)):
-    img = 255.0*eval_dataset[i][0].unsqueeze(-1)
-    img_folder = os.path.join("images", "healthy", f"{eval_targets[i]}")
-    if not os.path.exists(img_folder):
-        os.mkdir(img_folder)
-    imageio.imwrite(os.path.join(img_folder, "%08d.jpg" % i), np.repeat(img, repeats=3, axis=-1).type(torch.uint8))
-    print('test: ', i)
+# for i in range(len(eval_dataset)):
+#     img = 255.0*eval_dataset[i][0].unsqueeze(-1)
+#     img_folder = os.path.join("images", "healthy", f"{eval_targets[i]}")
+#     if not os.path.exists(img_folder):
+#         os.mkdir(img_folder)
+#     imageio.imwrite(os.path.join(img_folder, "%08d.jpg" % i), np.repeat(img, repeats=3, axis=-1).type(torch.uint8))
 
 data_transform = transforms.Compose([
     transforms.Grayscale(num_output_channels=3),
@@ -83,7 +81,7 @@ config = BaseTrainerConfig(
 
 model_config = VAEConfig(
     input_dim=(3, 128, 128),
-    latent_dim=16
+    latent_dim=128
 )
 
 model = VAE(
