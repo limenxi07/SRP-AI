bc2a0516dc80a26353b989f97d82c535a1758273
Lim En Xi <limenxi07@gmail.com>
2024-01-04 12:27:43 +0800

    changed dataset

--- VAEs/run.py
+++ VAEs/run.py
@@ -1,9 +1,8 @@
 import os
 
+import imageio
 import matplotlib.pyplot as plt
 import torch
-from torchvision import datasets, transforms
-
 from pythae.data.datasets import DatasetOutput
 from pythae.models import VAE, AutoModel, VAEConfig
 from pythae.models.nn.benchmarks.mnist import (Decoder_ResNet_AE_MNIST,
@@ -12,9 +11,44 @@ from pythae.pipelines.training import TrainingPipeline
 from pythae.samplers import (GaussianMixtureSampler,
                              GaussianMixtureSamplerConfig, NormalSampler)
 from pythae.trainers import BaseTrainerConfig
+from torchvision import datasets, transforms
 
 path = 'images'
 output = 'vae_healthy'
+
+# CUSTOM DATASET
+mnist_trainset = datasets.MNIST(root='../data', train=True, download=True, transform=None)
+
+train_dataset = mnist_trainset.data[:10000].reshape(-1, 1, 28, 28) / 255.
+train_targets = mnist_trainset.targets[:10000]
+eval_dataset = mnist_trainset.data[-10000:].reshape(-1, 1, 28, 28) / 255.
+eval_targets = mnist_trainset.targets[-10000:]
+
+if not os.path.exists("data_folders"):
+    os.mkdir("data_folders")
+if not os.path.exists("data_folders/train"):
+    os.mkdir("data_folders/train")
+if not os.path.exists("data_folders/eval"):
+    os.mkdir("data_folders/eval")
+
+for i in range(len(train_dataset)):
+    img = 255.0*train_dataset[i][0].unsqueeze(-1)
+    img_folder = os.path.join("data_folders", "train", f"{train_targets[i]}")
+    if not os.path.exists(img_folder):
+        os.mkdir(img_folder)
+    imageio.imwrite(os.path.join(img_folder, "%08d.jpg" % i), np.repeat(img, repeats=3, axis=-1).type(torch.uint8))
+
+for i in range(len(eval_dataset)):
+    img = 255.0*eval_dataset[i][0].unsqueeze(-1)
+    img_folder = os.path.join("data_folders", "eval", f"{eval_targets[i]}")
+    if not os.path.exists(img_folder):
+        os.mkdir(img_folder)
+    imageio.imwrite(os.path.join(img_folder, "%08d.jpg" % i), np.repeat(img, repeats=3, axis=-1).type(torch.uint8))
+
+data_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor() # the data must be tensors
+])
 
 class MyCustomDataset(datasets.ImageFolder):
 
@@ -28,11 +62,6 @@ class MyCustomDataset(datasets.ImageFolder):
             data=X
         )
 
-data_transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=3),
-    transforms.ToTensor() # the data must be tensors
-])
-
 train_dataset = MyCustomDataset(
     root=path,
     transform=data_transform,
@@ -44,11 +73,6 @@ eval_dataset = MyCustomDataset(
 )
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
-trainset = datasets.MNIST(root='../../data', train=True, download=True, transform=None)
-
-train_dataset = trainset.data[:-10000].reshape(-1, 1, 28, 28) / 255.
-eval_dataset = trainset.data[-10000:].reshape(-1, 1, 28, 28) / 255.
 
 config = BaseTrainerConfig(
     output_dir=output,
